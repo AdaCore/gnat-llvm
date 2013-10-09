0766c76c22ae9ac6dfc1cef00836b67e7e63292b
Raphael Amiard <amiard@adacore.com>
2013-10-09 11:20:15 +0200

    Boolean_Ops test : Use proper boolean type

--- testsuite/tests/boolean_ops/test.py
+++ testsuite/tests/boolean_ops/test.py
@@ -3,11 +3,10 @@ from gnatllvm import build_and_load, Func
 
 (boolean_ops, ) = build_and_load(
     ['boolean_ops.adb'], 'boolean_ops',
-    Func('boolean_ops', argtypes=[c_int, c_int], restype=c_int),
+    Func('boolean_ops', argtypes=[c_int, c_int], restype=c_bool),
 )
 
-print boolean_ops(1, 2)
-assert boolean_ops(1, 2) == 1
-assert boolean_ops(1, 1) == 0
-assert boolean_ops(-1, -2) == 0
-assert boolean_ops(-2, -1) == 1
+assert boolean_ops(1, 2) == True
+assert boolean_ops(1, 1) == False
+assert boolean_ops(-1, -2) == False
+assert boolean_ops(-2, -1) == True

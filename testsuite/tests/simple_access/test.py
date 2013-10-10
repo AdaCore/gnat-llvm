df577cee8c8c0fc217fe1cb5829e1e9c50d57052
Raphael Amiard <amiard@adacore.com>
2013-10-11 00:37:18 +0200

    Add test for access

---
+++ testsuite/tests/simple_access/test.py
@@ -0,0 +1,11 @@
+from ctypes import *
+from gnatllvm import build_and_load, Func
+
+(inc, ) = build_and_load(
+    ['inc.adb'], 'inc',
+    Func('inc', argtypes=[POINTER(c_int)], restype=None),
+)
+
+c = c_int(1)
+inc (c)
+assert c.value == 2

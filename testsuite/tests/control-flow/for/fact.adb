2b256719dd3901c60d1eea6065855db1a2eb9579
Pierre-Marie de Rodat <derodat@adacore.com>
2013-10-16 22:31:19 +0200

    Tests: fix the fact implementation...

--- testsuite/tests/control-flow/for/fact.adb
+++ testsuite/tests/control-flow/for/fact.adb
@@ -2,7 +2,7 @@ function Fact (N : Natural) return Natural is
    Result : Natural := 1;
 begin
    for I in 2 .. N loop
-      Result := Result * N;
+      Result := Result * I;
    end loop;
    return Result;
 end Fact;

3d8d5d7f9997f8c8e6a7dd794dd545c6b5ebd03a
Pierre-Marie de Rodat <derodat@adacore.com>
2014-03-16 18:15:15 +0100

    Add missing expected output and simplify the reversed FOR loops testcase

--- testsuite/tests/control-flow/for_reversed/count_digits.adb
+++ testsuite/tests/control-flow/for_reversed/count_digits.adb
@@ -1,28 +1,32 @@
 with Interfaces;
 
+use type Interfaces.Unsigned_8;
 use type Interfaces.Unsigned_32;
 
 function Count_Digits (X : Interfaces.Unsigned_32) return Natural is
    Digits_Set : constant
       array (Interfaces.Unsigned_32 range 0 .. 9)
       of Character := "0123456789";
-   Image      : array (1 .. 10) of Character;
+   Image      : array (Interfaces.Unsigned_8 range 1 .. 10) of Character;
    Tmp        : Interfaces.Unsigned_32 := X;
+
+   Result     : Integer := 1;
 begin
    for I in reverse Image'Range loop
-      Image (I) := Digits_Set (Tmp mod 10);
-      Tmp := Tmp / 10;
-   end loop;
+      declare
+         Next_Tmp : constant Interfaces.Unsigned_32 :=
+            Tmp / 10;
+         Current_Digit : constant Interfaces.Unsigned_32 :=
+            Tmp - 10 * Next_Tmp;
+      begin
+         Image (I) := Digits_Set (Current_Digit);
+         Tmp := Next_Tmp;
+      end;
 
-   for I in reverse Image'Range loop
-      if Image (I) = '0' then
-         if I = Image'Last then
-            return 1;
-         else
-            return Image'Last - I + 1;
-         end if;
+      if Image (I) /= '0' then
+         Result := Integer (Image'Last - I) + 1;
       end if;
    end loop;
 
-   return Image'Length;
+   return Integer'Max (1, Result);
 end Count_Digits;

f033b969cec4ceff88413ba8db1aec28e124801f
Arnaud Charlet <charlet@adacore.com>
2017-11-10 20:39:45 +0100

    (LLVMInitializeNativeTarget): Provide dummy symbol for now.

--- stubs/gcc_missing.c
+++ stubs/gcc_missing.c
@@ -20,6 +20,8 @@
  *                                                                          *
  ****************************************************************************/
 
+#include <stdlib.h>
+
 /* This file contains the C routines or variables which are defined in
    some GCC source (and hence not available when compiling here). */
 
@@ -61,3 +63,10 @@ update_path (char *path, char *key)
 unsigned char __gnat_separate_run_path_options = 0;
 const char *__gnat_default_libgcc_subdir = "lib";
 
+/* Provide dummy symbol to resolve link. */
+extern int
+LLVMInitializeNativeTarget (void)
+{
+  abort();
+  return 0;
+}

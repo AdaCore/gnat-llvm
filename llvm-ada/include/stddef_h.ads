1244d0e1f8857fd8472c025cb4bf21e4c6d9edeb
Arnaud Charlet <charlet@adacore.com>
2017-11-12 20:54:05 +0100

    Simplify binding and remove linux specific dependencies.
    no-precommit-check

--- llvm-ada/include/stddef_h.ads
+++ llvm-ada/include/stddef_h.ads
@@ -5,6 +5,7 @@ with Interfaces.C; use Interfaces.C;
 
 package stddef_h is
 
-   subtype size_t is unsigned_long;  -- /chelles.b/users/charlet/sandbox/x86_64-linux/gnat/install/lib/gcc/x86_64-pc-linux-gnu/6.4.1/include/stddef.h:216
+   subtype size_t is Interfaces.C.size_t;
+   subtype off_t is long;
 
 end stddef_h;

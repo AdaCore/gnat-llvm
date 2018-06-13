b96944ea155ced60cb744f9c9dd5fe30069ea224
Arnaud Charlet <charlet@adacore.com>
2018-06-13 13:03:49 +0200

    Remove obsolete tests, not compatible with full runtime

--- testsuite/tests/accesses/new/n.adb
+++
@@ -1,7 +0,0 @@
-function N return Natural is
-   type Nat_Access is access all Natural;
-   A : Nat_Access := new Natural;
-begin
-   A.all := 123;
-   return A.all;
-end;
--- testsuite/tests/accesses/new/test.py
+++
@@ -1,9 +0,0 @@
-from ctypes import *
-from gnatllvm import build_and_load, Func
-
-(n, ) = build_and_load(
-    ['n.adb'], 'n',
-    Func('_ada_n', argtypes=[], restype=c_int),
-)
-
-print n()

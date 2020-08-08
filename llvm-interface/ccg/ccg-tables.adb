5c7fd3140cd0a6ce24e20dd298017fc298f46392
Richard Kenner <kenner@adacore.com>
2020-08-08 18:13:27 -0400

    Add structure to record decls and statements
    Change-Id: Iecfcdc0d16e7cc2264dc1f7941926622fe962eb8

--- llvm-interface/ccg/ccg-tables.adb
+++ llvm-interface/ccg/ccg-tables.adb
@@ -210,6 +210,61 @@ package body CCG.Tables is
      with Pre => Present (T), Post => Get_Is_Typedef_Output (T);
    --  See if we need to write a typedef for T and write one if so
 
+   type Decl_Idx is new Nat;
+   type Stmt_Idx is new Nat;
+
+   No_Decl_Idx : constant Decl_Idx := 0;
+   No_Stmt_Idx : constant Stmt_Idx := 0;
+
+   function No (J : Decl_Idx) return Boolean is (J = No_Decl_Idx);
+   function No (J : Stmt_Idx) return Boolean is (J = No_Stmt_Idx);
+
+   --  Current (last) and first indices for decls and statement in the
+   --  current subprogram.
+
+   First_Decl : Decl_Idx := No_Decl_Idx;
+   Last_Decl  : Decl_Idx := No_Decl_Idx;
+   First_Stmt : Stmt_Idx := No_Stmt_Idx;
+   Last_Stmt  : Stmt_Idx := No_Stmt_Idx;
+
+   --  Tables for decls and statements
+
+   package Decl_Table is new Table.Table
+     (Table_Component_Type => Str,
+      Table_Index_Type     => Decl_Idx,
+      Table_Low_Bound      => 1,
+      Table_Initial        => 500,
+      Table_Increment      => 100,
+      Table_Name           => "Decl_Table");
+
+   package Stmt_Table is new Table.Table
+     (Table_Component_Type => Str,
+      Table_Index_Type     => Stmt_Idx,
+      Table_Low_Bound      => 1,
+      Table_Initial        => 1000,
+      Table_Increment      => 1000,
+      Table_Name           => "Stmt_Table");
+
+   --  For each subprogram, we record the first and last decl and statement
+   --  belonging to that subprogram.
+
+   type Subprogram_Data is record
+      First_Decl : Decl_Idx;
+      Last_Decl  : Decl_Idx;
+      First_Stmt : Stmt_Idx;
+      Last_Stmt  : Stmt_Idx;
+   end record;
+
+   type Subprogram_Idx is new Nat;
+
+   package Subprogram_Table is new Table.Table
+     (Table_Component_Type => Subprogram_Data,
+      Table_Index_Type     => Subprogram_Idx,
+      Table_Low_Bound      => 1,
+      Table_Initial        => 50,
+      Table_Increment      => 50,
+      Table_Name           => "Subprogram_Table");
+
    -----------------
    -- Update_Hash --
    ----------------
@@ -1127,6 +1182,44 @@ package body CCG.Tables is
 
       return BBD.Output_Idx;
    end Get_Output_Idx;
+
+   -----------------
+   -- Output_Decl --
+   ----------------
+
+   procedure Output_Decl (S : Str) is
+   begin
+      Decl_Table.Append (S);
+      Last_Decl := Decl_Table.Last;
+      if No (First_Decl) then
+         First_Decl := Decl_Table.Last;
+      end if;
+   end Output_Decl;
+
+   -----------------
+   -- Output_Stmt --
+   ----------------
+
+   procedure Output_Stmt (S : Str) is
+   begin
+      Stmt_Table.Append (S);
+      Last_Stmt := Stmt_Table.Last;
+      if No (First_Stmt) then
+         First_Stmt := Stmt_Table.Last;
+      end if;
+   end Output_Stmt;
+
+   --------------------
+   -- New_Subprogram --
+   --------------------
+
+   procedure New_Subprogram is
+   begin
+      Subprogram_Table.Append ((First_Decl => First_Decl,
+                                Last_Decl  => Last_Decl,
+                                First_Stmt => First_Stmt,
+                                Last_Stmt  => Last_Stmt));
+   end New_Subprogram;
 
    ------------------------
    --  Initialize_Tables --

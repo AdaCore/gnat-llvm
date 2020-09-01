d2ee442cb341181e50a1236940cc65935d7f39fc
Richard Kenner <kenner@adacore.com>
2020-09-01 09:11:17 -0400

    Use proper boolean type when calling C++ API
    Change-Id: I2b97d8d92e44487555a2305b198b92f170ab1474

--- llvm-interface/ccg/ccg-helper.adb
+++ llvm-interface/ccg/ccg-helper.adb
@@ -40,7 +40,7 @@ package body CCG.Helper is
    --------------
 
    function Has_Name (T : Type_T) return Boolean is
-      function Has_Name (T : Type_T) return Bool_T
+      function Has_Name (T : Type_T) return Bool
         with Import, Convention => C, External_Name => "Has_Name";
    begin
       return (if Has_Name (T) /= 0 then True else False);

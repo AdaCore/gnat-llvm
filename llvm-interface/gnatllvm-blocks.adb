f224fcff5183848403b7d09b69a1e5397e9e7a7b
Richard Kenner <kenner@adacore.com>
2020-10-26 17:03:48 -0400

    Add missing tests for unreferenced local EH's; fixes TA15-022
    Change-Id: I4f1cbb4ba883caa34f58c0e694bc5bf936062c08

--- llvm-interface/gnatllvm-blocks.adb
+++ llvm-interface/gnatllvm-blocks.adb
@@ -1427,7 +1427,22 @@ package body GNATLLVM.Blocks is
    -- Process_Push_Pop_xxx_Error_Label --
    --------------------------------------
 
-   procedure Process_Push_Pop_xxx_Error_Label (N : Node_Id) is begin
+   procedure Process_Push_Pop_xxx_Error_Label (N : Node_Id) is
+      procedure Maybe_Warn (E : Entity_Id);
+      --  Warn if we haven't generated a branch to E
+
+      ----------------
+      -- Maybe_Warn --
+      ----------------
+
+      procedure Maybe_Warn (E : Entity_Id) is
+      begin
+         if No (Get_Label_Info (E)) then
+            Warn_If_No_Local_Raise (E);
+         end if;
+      end Maybe_Warn;
+
+   begin
       case Nkind (N) is
          when N_Push_Constraint_Error_Label =>
             Constraint_Error_Stack.Append (Exception_Label (N));
@@ -1439,12 +1454,18 @@ package body GNATLLVM.Blocks is
             Program_Error_Stack.Append (Exception_Label (N));
 
          when N_Pop_Constraint_Error_Label =>
+            Maybe_Warn
+              (Constraint_Error_Stack.Table (Constraint_Error_Stack.Last));
             Constraint_Error_Stack.Decrement_Last;
 
          when N_Pop_Storage_Error_Label =>
+            Maybe_Warn
+              (Storage_Error_Stack.Table (Storage_Error_Stack.Last));
             Storage_Error_Stack.Decrement_Last;
 
          when N_Pop_Program_Error_Label =>
+            Maybe_Warn
+              (Program_Error_Stack.Table (Program_Error_Stack.Last));
             Program_Error_Stack.Decrement_Last;
 
          when others =>

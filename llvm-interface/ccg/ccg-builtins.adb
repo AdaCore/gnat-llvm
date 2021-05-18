5ce6a8fd0b87f706a33237948b76b655b9da7b2f
Richard Kenner <kenner@adacore.com>
2021-05-18 08:38:24 -0400

    Remove unneeded with's
    Change-Id: Ie71e87de6308129f0aafae70523b1a75f0135cca

--- llvm-interface/ccg/ccg-builtins.adb
+++ llvm-interface/ccg/ccg-builtins.adb
@@ -18,16 +18,10 @@
 with Ada.Characters.Handling; use Ada.Characters.Handling;
 
 with Output; use Output;
-with Table;
 
-with GNATLLVM.Wrapper; use GNATLLVM.Wrapper;
-
-with CCG.Aggregates;   use CCG.Aggregates;
-with CCG.Blocks;       use CCG.Blocks;
 with CCG.Instructions; use CCG.Instructions;
 with CCG.Output;       use CCG.Output;
 with CCG.Subprograms;  use CCG.Subprograms;
-with CCG.Tables;       use CCG.Tables;
 with CCG.Utils;        use CCG.Utils;
 
 package body CCG.Builtins is

32c82b5ec11bf4a859f7c8493b9f83fb69c7b27f
Richard Kenner <kenner@adacore.com>
2021-12-17 08:49:37 -0500

    Add missing annotation for debug helpers
    Change-Id: Id00746bdd18ced86cfb65769675b484d288f581b

--- llvm-interface/gnatllvm-utils.ads
+++ llvm-interface/gnatllvm-utils.ads
@@ -166,6 +166,8 @@ package GNATLLVM.Utils is
    procedure Scan_Library_Item (U : Node_Id);
    --  Procedure to scan all library units calling the parameter for each
 
+   pragma Annotate (Xcov, Exempt_On, "Debug helpers");
+
    procedure Dump_LLVM_Value (V : Value_T)
      with Export, External_Name => "dllv";
    --  Simple wrapper around LLVM.Core.Dump_Value. Gives an Ada name to this
@@ -180,5 +182,7 @@ package GNATLLVM.Utils is
 
    procedure Dump_LLVM_Metadata (MD : Metadata_T)
      with Export, External_Name => "dllm";
+
+   pragma Annotate (Xcov, Exempt_Off, "Debug helpers");
 
 end GNATLLVM.Utils;

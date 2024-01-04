7ba01eef59469bf3b717d7e5a022b2ebe12ed7c4
Sebastian Poeplau <poeplau@adacore.com>
2024-01-04 10:39:01 +0100

    Don't delete callback objects before accessing them
    We also drop the value pointer; it's redundant because the base class
    already stores it. Fixes eng/toolchain/gnat-llvm#86.

--- llvm-interface/llvm_wrapper.cc
+++ llvm-interface/llvm_wrapper.cc
@@ -1151,7 +1151,6 @@ Is_Lifetime_Intrinsic (Instruction *v)
 
 class GNATCallbackVH final : public CallbackVH
 {
-  Value *val;
   void (*fn) (Value *);
   void deleted () override;
 
@@ -1162,12 +1161,15 @@ class GNATCallbackVH final : public CallbackVH
 void
 GNATCallbackVH::deleted()
 {
+  (this->fn) (getValPtr());
+
+  // This callback object is no longer needed, and we don't keep references to
+  // it anywhere, so self-delete.
   delete this;
-  (this->fn) (this->val);
 }
 
 GNATCallbackVH::GNATCallbackVH (Value *V, void (*Fn) (Value *))
-  : CallbackVH (V), val (V), fn (Fn)
+  : CallbackVH (V), fn (Fn)
 {
 }
 

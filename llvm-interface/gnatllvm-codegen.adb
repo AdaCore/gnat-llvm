d6625264196904c1419ab27d3cf99413339eb5fc
Sebastian Poeplau <poeplau@adacore.com>
2024-02-20 14:27:43 +0100

    Emit a top-level exception handler on Windows
    Windows Structured Exception Handling requires us to catch unhandled
    exceptions in a handler around the main subprogram. Since the frontend
    doesn't emit it as part of the tree, we're expected to generate it. We
    mirror the gcc-interface behavior of emitting a clause that checks for
    exception type __gnat_unhandled_others_value (which receives special
    treatment in our runtime's SEH personality function); the handler calls
    the special-purpose runtime function __gnat_unhandled_except_handler.
    
    Fixes eng/toolchain/gnat-llvm#104 and eng/toolchain/gnat-llvm#105.

--- llvm-interface/gnatllvm-codegen.adb
+++ llvm-interface/gnatllvm-codegen.adb
@@ -566,6 +566,8 @@ package body GNATLLVM.Codegen is
                         Get_LLVM_Error_Msg (Ptr_Err_Msg));
       end if;
 
+      SEH := Has_SEH (Normalized_Target_Triple.all);
+
       if not PIC_PIE_Set then
          --  Set the same target-dependent default as Clang: PIE level 2 for
          --  Linux and 64-bit Windows, and neither PIC nor PIE otherwise. (The
